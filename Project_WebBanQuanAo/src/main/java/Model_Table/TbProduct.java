29e4dce6a0e5e4624539ce634f1423b6345e9efe
HuynhTrongNghia <huynhtrongnghia1090@gmail.com>
2020-11-19 23:04:16 +0700

    Connect Data

--- Project_WebBanQuanAo/src/main/java/Model_Table/TbProduct.java
+++ Project_WebBanQuanAo/src/main/java/Model_Table/TbProduct.java
@@ -2,67 +2,66 @@ package Model_Table;
 
 public class TbProduct {
     private int id;
+    private String image;
     private String name;
     private float price;
     private int cate_id;
     private String des;
-    private String image;
 
-    public TbProduct(int id,String name,float price,int cate_id,String des,String image)
-    {
-        this.id=id;
-        this.name=name;
-        this.price=price;
-        this.cate_id=cate_id;
-        this.des=des;
-        this.image=image;
-    }
-
-    public void setId(int id) {
+    public TbProduct(int id, String image, String name, float price, int cate_id, String des) {
         this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public void setCate_id(int cate_id) {
-        this.cate_id = cate_id;
-    }
-
-    public void setDes(String des) {
-        this.des = des;
-    }
-
-    public void setImage(String image) {
         this.image = image;
+        this.name = name;
+        this.price = price;
+        this.cate_id = cate_id;
+        this.des = des;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public float getPrice() {
         return price;
     }
 
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
     public int getCate_id() {
         return cate_id;
+    }
+
+    public void setCate_id(int cate_id) {
+        this.cate_id = cate_id;
     }
 
     public String getDes() {
         return des;
     }
 
-    public String getImage() {
-        return image;
+    public void setDes(String des) {
+        this.des = des;
     }
 }
